c0f16a2a1663754a4627394084c22f75b167c8cf
Thomas HINGANT <thomas.hingant@live.fr>
2015-02-03 12:01:00 +0100

    Ajout des boutons de sélection des caractères

--- app/src/main/java/fr/eseo/momix/AnswerValidator.java
+++ app/src/main/java/fr/eseo/momix/AnswerValidator.java
@@ -1,38 +1,22 @@
 package fr.eseo.momix;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class AnswerValidator implements TextWatcher {
+public class AnswerValidator implements View.OnClickListener {
 
     private MainActivity activity;
     private Anagram anagram;
 
     public AnswerValidator(MainActivity activity) {
         this.activity = activity;
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        EditText answerField = (EditText) activity.findViewById(R.id.answer);
-        String answer = answerField.getText().toString();
-        if(answer.compareTo(anagram.getAnswer()) == 0) {
-            TextView wonText = (TextView) activity.findViewById(R.id.won);
-            wonText.setVisibility(View.VISIBLE);
-        }
     }
 
     public Anagram getAnagram() {
@@ -42,4 +26,110 @@ public class AnswerValidator implements TextWatcher {
     public void setAnagram(Anagram anagram) {
         this.anagram = anagram;
     }
+
+    @Override
+    public void onClick(View v) {
+
+        ToggleButton b = (ToggleButton) v;
+
+        TextView answerField = (TextView) activity.findViewById(R.id.answer);
+        String answer = answerField.getText().toString();
+        char[] input = stringToChar(answer);
+        char[] output = new char[input.length];
+        char characterSelected = b.getText().toString().charAt(0);
+
+        int k;
+        if(b.isChecked()) {
+            k = 0;
+            boolean characterFound = false;
+            while(!characterFound && k < input.length) {
+                characterFound = input[k] == ' ' && anagram.getAnswer().charAt(k) != ' ';
+                k++;
+            }
+            if(characterFound) {
+                int indexInserted = k-1;
+                output[indexInserted] = characterSelected;
+                for(; k < input.length ; k++) {
+                    output[k] = input[k];
+                }
+                for(k = indexInserted-1;k >= 0 ; k--) {
+                    output[k] = input[k];
+                }
+            } else {
+                for(k = 0;k < input.length ; k++) {
+                    output[k] = input[k];
+                }
+            }
+        } else {
+            k = input.length - 1;
+            boolean characterFound = false;
+            while(!characterFound && k >= 0) {
+                characterFound = characterSelected == input[k];
+                k--;
+            }
+            if(characterFound) {
+                int indexDeleted = k+1;
+                for(;k >= 0 ; k--) {
+                    output[k] = input[k];
+                }
+                for(k = indexDeleted; k < input.length-1 ; k++) {
+                    if(anagram.getAnswer().charAt(k+1) == ' ') {
+                        output[k] = input[k+2];
+                        output[k+1] = ' ';
+                        k++;
+                    } else {
+                        output[k] = input[k + 1];
+                    }
+                }
+                output[k] = ' ';
+            } else {
+                for(k = 0;k < input.length ; k++) {
+                    output[k] = input[k];
+                }
+            }
+        }
+        String actualAnswer = charToString(output);
+        answerField.setText(charToString(output));
+
+        if(actualAnswer.compareTo(anagram.getAnswer()) == 0) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+            builder.setMessage(R.string.won_content)
+                    .setTitle(R.string.won);
+            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    activity.generateAnagram();
+                }
+            });
+            builder.setNegativeButton(R.string.back, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+                }
+            });
+
+            // 3. Get the AlertDialog from create()
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
+
+    }
+
+    private char[] stringToChar(String s) {
+        char[] text = new char[s.length()];
+        for (int i=0; i<s.length(); i++) {
+            text[i] = s.charAt(i);
+        }
+        return text;
+    }
+
+    private String charToString(char[] c) {
+        String s = "";
+        for (int i=0; i<c.length; i++) {
+            s+= c[i];
+        }
+        return s;
+    }
+
 }
--- app/src/main/java/fr/eseo/momix/MainActivity.java
+++ app/src/main/java/fr/eseo/momix/MainActivity.java
@@ -2,25 +2,42 @@ package fr.eseo.momix;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.widget.GridLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends ActionBarActivity {
 
     private AnswerValidator validator;
+    private int screenWidth;
+    private int screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get screen width and height
+        if(android.os.Build.VERSION.SDK_INT < 10) {
+            Display display = getWindowManager().getDefaultDisplay();
+            screenWidth = display.getWidth();
+            screenHeight = display.getHeight();
+        } else {
+            DisplayMetrics metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            screenWidth = metrics.widthPixels;
+            screenHeight = metrics.heightPixels;
+        }
+        screenWidth-= dpToPx(2 * 16);
+        screenHeight-= dpToPx(2 * 16);
+
+        // Generate anagram
         generateAnagram();
 
-        EditText answer = (EditText) findViewById(R.id.answer);
-        answer.addTextChangedListener(validator);
     }
 
 
@@ -33,31 +50,93 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_dictionnary) {
             return true;
         } else if(id == R.id.action_new) {
             generateAnagram();
-
-            TextView wonText = (TextView) findViewById(R.id.won);
-            wonText.setVisibility(View.INVISIBLE);
-            TextView anagram = (TextView) findViewById(R.id.anagram);
-            anagram.setText(validator.getAnagram().getGenerated());
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void generateAnagram() {
+    public void generateAnagram() {
+
+        // Creation of the anagram
         Anagram a = new Anagram("Le petit chaperon rouge");
         validator = new AnswerValidator(this);
         validator.setAnagram(a);
+
+        // Setup of the anagram on the view
+        TextView anagram = (TextView) findViewById(R.id.anagram);
+        anagram.setText(validator.getAnagram().getGenerated());
+
+        // Initialisation of the answer
+        TextView answerView = (TextView) findViewById(R.id.answer);
+        String answer = "";
+        for(int k = 0 ; k < validator.getAnagram().getAnswer().length() ; k++) {
+            answer+= ' ';
+        }
+        answerView.setText(answer);
+
+        // Remove previous buttons
+        GridLayout lettersLayout = (GridLayout) findViewById(R.id.letters);
+        lettersLayout.removeAllViews();
+
+        // Setup of the answer buttons
+        int x, y;
+        int kButton = 0, kLetter = 0;
+        char c = 0;
+        int lengthString = validator.getAnagram().getGenerated().length();
+        while(kLetter < lengthString) {
+
+            // Add a button only if it is not a space
+            do {
+                c = validator.getAnagram().getGenerated().charAt(kLetter);
+                kLetter++;
+            } while(c == ' ' && kLetter < lengthString);
+
+            // Creation of the button
+            ToggleButton letter = new ToggleButton(this);
+
+            // Parameters of the buttons
+            x = kButton % (screenWidth / 105);
+            y = kButton / (screenWidth / 105);
+            GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+            param.height = GridLayout.LayoutParams.WRAP_CONTENT;
+            param.width = 100;
+            param.rightMargin = 5;
+            param.topMargin = 5;
+            param.columnSpec = GridLayout.spec(x);
+            param.rowSpec = GridLayout.spec(y);
+            letter.setLayoutParams(param);
+
+            letter.setText("" + c);
+            letter.setTextOn("" + c);
+            letter.setTextOff("" + c);
+            letter.setOnClickListener(validator);
+
+            lettersLayout.addView(letter);
+
+            kButton++;
+        }
+
+    }
+
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
+    }
+
+    private char[] stringToChar(String s) {
+        char[] text = new char[s.length()];
+        for (int i=0; i<s.length(); i++) {
+            text[i] = s.charAt(i);
+        }
+        return text;
     }
 
 }
